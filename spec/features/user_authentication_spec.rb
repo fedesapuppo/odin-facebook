ea58795e4f459694026c254b6cfd043bd0cf3cb3
Federico Sapuppo <fedesapuppo@hotmail.com>
2023-10-06 19:03:12 +0300

    modify layout and add auth test
    add notice and alerts to layouts/application.html.erb, implement
    integration capybara test user_authentication_spec

---
+++ spec/features/user_authentication_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.feature "User Authentication", type: :feature do
+  scenario "User signs up, logs in, and logs out" do
+    visit new_user_registration_path
+
+    fill_in "user_email", with: "johndoe@example.com"
+    fill_in "user_password", with: "password"
+    fill_in "user_password_confirmation", with: "password"
+    click_button "Sign up"
+
+    expect(page).to have_content("Welcome! You have signed up successfully.")
+
+    click_button "Sign Out"
+    expect(page).to have_content("Signed out successfully.")
+
+    visit new_user_session_path
+    fill_in "user_email", with: "johndoe@example.com"
+    fill_in "user_password", with: "password"
+    click_button "Log in"
+
+    expect(page).to have_content("Logged in as johndoe@example.com")
+  end
+end
