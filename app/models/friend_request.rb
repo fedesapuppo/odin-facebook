04c57421bebc00342f01d2a51951666fa301505a
Federico Sapuppo <fedesapuppo@hotmail.com>
2023-10-12 18:11:21 +0300

    change FriendRequest status from string to enum

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -2,7 +2,7 @@ class FriendRequest < ApplicationRecord
   belongs_to :requester, class_name: "User", foreign_key: "requester_id"
   belongs_to :receiver, class_name: "User", foreign_key: "receiver_id"
 
-  validates :status, presence: true, inclusion: { in: ["pending", "accepted", "rejected"] }
+  validates :status, presence: true, inclusion: { in: [0, 1, 2] }
 
-  scope :pending_requests, -> { where(status: "pending") }
+  enum :status, { pending: 0, accepted: 1, rejected: 2 }, prefix: true, default: :pending
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_12_083934) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_12_142749) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,7 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_12_083934) do
   create_table "friend_requests", force: :cascade do |t|
     t.integer "requester_id"
     t.integer "receiver_id"
-    t.string "status"
+    t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
