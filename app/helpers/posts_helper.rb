ae2f524f6c631ab38f744bc222ece67a51ae701b
Federico Sapuppo <fedesapuppo@hotmail.com>
2024-03-04 10:27:34 -0300

    complete like and unlike post feature

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,8 @@ module PostsHelper
   def liked_by?(post, _user_id)
     post.likes.exists?(user_id: current_user.id)
   end
+
+  def current_user_like(post)
+    post.likes.find_by(user_id: current_user.id)
+  end
 end
