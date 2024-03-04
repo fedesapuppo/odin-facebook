031205481cad8c62bb7218356b70400c78f4cd83
Federico Sapuppo <fedesapuppo@hotmail.com>
2024-03-04 10:51:30 -0300

    implement users can like posts feature

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,6 @@
 module PostsHelper
   def liked_by?(post, _user_id)
-    post.likes.exists?(user_id: current_user.id)
+    post.likes.exists?(user: current_user)
   end
 
   def current_user_like(post)
