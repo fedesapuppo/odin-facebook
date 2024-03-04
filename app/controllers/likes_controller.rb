fd2cb6733dcc062c48e4ac52db8acf375efa07cd
Federico Sapuppo <fedesapuppo@hotmail.com>
2024-03-04 11:26:11 -0300

    update flash messages

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,12 +5,13 @@ class LikesController < ApplicationController
     @like = current_user.likes.find_or_initialize_by(post_id: params[:post_id])
     if @like.new_record?
       if @like.save
-        flash[:success] = 'Post liked.'
+        flash[:notice] = 'Post liked.'
+
       else
-        flash[:error] = 'Unable to like the post.'
+        flash[:alert] = 'Unable to like the post.'
       end
     else
-      flash[:error] = 'You have already liked this post.'
+      flash[:alert] = 'You have already liked this post.'
     end
     redirect_to posts_path
   end
@@ -19,9 +20,9 @@ class LikesController < ApplicationController
     @like = current_user.likes.find_by(post: @post)
     if @like
       @like.destroy
-      flash[:success] = 'Like removed.'
+      flash[:notice] = 'Like removed.'
     else
-      flash[:error] = 'Unable to remove the like.'
+      flash[:alert] = 'Unable to remove the like.'
     end
     redirect_to posts_path
   end
