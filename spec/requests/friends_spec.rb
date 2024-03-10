13a43233683e3d4f712dc8c6243c23151919ecc2
Federico Sapuppo <fedesapuppo@hotmail.com>
2024-03-10 19:34:54 -0300

    update friends requests spec

--- spec/requests/friends_spec.rb
+++ spec/requests/friends_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe "Friends", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/friends/index"
+RSpec.describe 'Friends', type: :request do
+  include Devise::Test::IntegrationHelpers
+  describe 'GET /index' do
+    it 'returns http success' do
+      user = create(:user)
+      sign_in user
+      get '/friends/index'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
