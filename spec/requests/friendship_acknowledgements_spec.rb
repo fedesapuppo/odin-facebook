f143e458bcb7824177183b9c8fdbdb6d523e5205
Federico Sapuppo <fedesapuppo@hotmail.com>
2024-03-10 20:28:23 -0300

    friendship_acknowledgements_spec wip

--- spec/requests/friendship_acknowledgements_spec.rb
+++ spec/requests/friendship_acknowledgements_spec.rb
@@ -1,18 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe "FriendshipAcknowledgements", type: :request do
-  describe "GET /create" do
-    it "returns http success" do
-      get "/friendship_acknowledgements/create"
-      expect(response).to have_http_status(:success)
+RSpec.describe 'FriendshipAcknowledgements', type: :request do
+  include Devise::Test::IntegrationHelpers
+  describe 'POST /create' do
+    it 'redirects to notifications path' do
+      user = create(:user)
+      friend_request = create(:friend_request)
+      sign_in user
+      post '/friendship_acknowledgements', params: { friend_request: friend_request.id }
+      expect(response).to redirect_to(notifications_path)
     end
   end
 
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/friendship_acknowledgements/destroy"
-      expect(response).to have_http_status(:success)
+  describe 'DELETE /destroy' do
+    it 'redirects to notifications path' do
+      user = create(:user)
+      sign_in user
+      friendship_acknowledgement = create(:friendship_acknowledgement)
+      delete "/friendship_acknowledgements/#{friendship_acknowledgement.id}"
+      expect(response).to redirect_to(notifications_path)
     end
   end
-
 end
