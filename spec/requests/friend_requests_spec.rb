3142be408162433ce09a0b6f2f8d650e130da1a4
Federico Sapuppo <fedesapuppo@hotmail.com>
2024-03-11 19:20:24 -0300

    Update spec/requests/friend_requests_spec.rb
    Co-authored-by: Tute Costa <tutecosta@gmail.com>

--- spec/requests/friend_requests_spec.rb
+++ spec/requests/friend_requests_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe 'FriendRequests', type: :request do
   include Devise::Test::IntegrationHelpers
   describe 'POST /create' do
-    it 'returns http success' do
+    context 'not logged in' do
+      it 'redirects to root' do
       user = create(:user)
       sign_in user
       post '/friend_requests/create', params: { friend_request: { receiver_id: user.id } }
