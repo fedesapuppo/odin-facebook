27112b8eb5cd365e26787975d383c9c8122d7aaf
Federico Sapuppo <fedesapuppo@hotmail.com>
2024-03-13 10:40:05 -0300

    friend_requests_spec.rb

--- spec/requests/friend_requests_spec.rb
+++ spec/requests/friend_requests_spec.rb
@@ -3,19 +3,43 @@ require 'rails_helper'
 RSpec.describe 'FriendRequests', type: :request do
   include Devise::Test::IntegrationHelpers
 
+  let(:requester) { create(:user) }
+  let(:receiver) { create(:user) }
+  let(:friend_request) { create(:friend_request) }
+
+  before do
+    sign_in requester
+  end
+
   describe 'POST /create' do
     context 'logged in' do
       it 'redirects to root' do
-        user = create(:user)
-        sign_in user
-        post '/friend_requests/', params: { friend_request: { receiver_id: user.id } }
+        post '/friend_requests/', params: { friend_request: { receiver_id: receiver.id } }
         expect(response).to redirect_to(root_path)
       end
     end
 
     context 'not logged in' do
       it 'redirects to sign in page' do
-        post '/friend_requests/', params: { friend_request: { receiver_id: 1 } }
+        sign_out requester
+        post '/friend_requests/', params: { friend_request: { receiver_id: receiver.id } }
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
+  describe 'DELETE /destroy' do
+    context 'logged in' do
+      it 'redirects to root' do
+        delete "/friend_requests/#{friend_request.id}"
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context 'not logged in' do
+      it 'redirects to sign in page' do
+        sign_out requester
+        delete "/friend_requests/#{friend_request.id}"
         expect(response).to redirect_to(new_user_session_path)
       end
     end
