4ae6265f4e50a1184adaeea4aae1f8e9b0c120b0
Federico Sapuppo <fedesapuppo@hotmail.com>
2024-03-13 13:21:52 -0300

    refactor friendship_acknoledgements_controller.rb

--- app/controllers/friendship_acknowledgements_controller.rb
+++ app/controllers/friendship_acknowledgements_controller.rb
@@ -1,26 +1,33 @@
 class FriendshipAcknowledgementsController < ApplicationController
   def create
-    friend_request = FriendRequest.find(params[:friend_request][:friend_request_id])
-
-    ActiveRecord::Base.transaction do
-      friend_request.update(status: "accepted")
-      friendship1 = Friendship.create(user: friend_request.receiver, friend: friend_request.requester)
-      friendship2 = Friendship.create(user: friend_request.requester, friend: friend_request.receiver)
-      FriendshipAcknowledgement.create(friend_request_id: friend_request.id, friendship_id: [friendship1, friendship2])
-    end
-
-    redirect_to notifications_path
+    friend_request = find_friend_request
+    acknowledge_friend_request(friend_request, "accepted")
   end
 
   def destroy
-    friend_request = FriendRequest.find(params[:friend_request][:friend_request_id])
-
-    friend_request.update(status: "rejected")
-
-    redirect_to notifications_path
+    friend_request = find_friend_request
+    acknowledge_friend_request(friend_request, "rejected")
   end
 
   private
+
+  def find_friend_request
+    FriendRequest.find(params[:friend_request][:friend_request_id])
+  end
+
+  def acknowledge_friend_request(friend_request, status)
+    ActiveRecord::Base.transaction do
+      friend_request.update(status: status)
+      create_friendships(friend_request) if status == "accepted"
+    end
+    redirect_to notifications_path
+  end
+
+  def create_friendships(friend_request)
+    friendship1 = Friendship.create(user: friend_request.receiver, friend: friend_request.requester)
+    friendship2 = Friendship.create(user: friend_request.requester, friend: friend_request.receiver)
+    FriendshipAcknowledgement.create(friend_request_id: friend_request.id, friendship_id: [friendship1, friendship2])
+  end
 
   def friendship_acknowledgement_params
     params.require(:friend_request).permit(:friend_request_id)
