bd0342125675da249dde9a0b8fcc68d536c56ccd
Federico Sapuppo <fedesapuppo@hotmail.com>
2024-03-15 10:01:05 -0300

    notifications_spec.rb

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,5 @@
 class NotificationsController < ApplicationController
+  before_action :authenticate_user!
   def index
     @friend_requests = FriendRequest.where(receiver_id: current_user, status: 'pending')
   end
