5919b5efd6a0f4aac1c48c0adb1b61482db626b5
Federico Sapuppo <fedesapuppo@hotmail.com>
2024-03-20 11:51:18 -0300

    friend_request_spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:content) }
+  end
+
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:post) }
+  end
+
+  describe 'factory' do
+    it 'is valid' do
+      expect(build(:comment)).to be_valid
+    end
+  end
 end
