e6d6bad0b109113ab231b4420ca28aa4d599d395
Federico Sapuppo <fedesapuppo@hotmail.com>
2024-03-26 11:42:01 -0300

    user_send_friend_request_spec

--- spec/features/user_send_friend_request_spec.rb
+++ spec/features/user_send_friend_request_spec.rb
@@ -2,24 +2,26 @@ require 'rails_helper'
 
 RSpec.feature 'Users can send and unsend friend requests to other users', type: :feature do
   context 'User logs in, visits friends page, and send follow request' do
-    let(:user) { create(:user) }
-    let(:non_friend) { create(:user) }
-    let(:users_not_friends) { User.where.not(id: user.friends).where.not(id: user.id) }
+    scenario 'User sends a friend request to another user' do
+      user = create(:user)
+      non_friend = create(:user)
 
-    before do
       sign_in user
       visit friends_path
-    end
+      click_button 'Send Friend Request'
 
-    scenario 'User sends a friend request to another user' do
-
-      #click_button 'Send Friend Request'
-
-      #expect(page).to have_content('Friend request sent!')
+      expect(page).to have_content('Friend request sent!')
       expect(page).to have_content(non_friend.name)
     end
 
     scenario 'User unsends a friend request to another user' do
+      user = create(:user)
+      non_friend = create(:user)
+      friend_request = create(:friend_request, requester: user, receiver: non_friend)
+      friend_requests = FriendRequest.where(requester_id: user, status: 'pending')
+
+      sign_in user
+      visit friends_path
       click_button 'Unsend Friend Request'
 
       expect(page).to have_content('Friend request cancelled')
