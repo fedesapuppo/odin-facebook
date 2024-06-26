ed91f41a16854ebf431b1a191ce80f323ec50d04
Federico Sapuppo <fedesapuppo@hotmail.com>
2024-06-26 17:08:25 -0300

    mailer working with gmail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: 'notifications@example.com'
+  default from: 'notifications@odin-facebook.com'
 
   def welcome_email
     @user = params[:user]
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,8 +41,24 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.perform_deliveries = true
+  # Letter_opener mailer configuration
+  #config.action_mailer.delivery_method = :letter_opener
+  #config.action_mailer.perform_deliveries = true
+
+  # gmail mailer configuration
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: '587',
+    domain: 'gmail.com',
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    user_name: ENV.fetch('gmail_sender_adress'),
+    password: ENV.fetch('gmail_sender_password')
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
